cc8b5b5866cce9ef36e7f20e3ca8ac802a302144
Nicolas Spogis <nicolas.spogis@gmail.com>
2023-10-30 20:36:58 -0300

    Update

--- YouTubeExtract.py
+++ YouTubeExtract.py
@@ -15,12 +15,13 @@ import matplotlib.pyplot as plt
 from wordcloud import WordCloud, STOPWORDS
 from selenium import webdriver
 import time
+import datetime
 
 global MinChar
 global chrome_path
 MinChar = 2
 global MaxHeight
-MaxHeight = 10
+MaxHeight = 20
 
 chrome_path = r'C:/bin/chromedriver.exe'
 
@@ -137,7 +138,7 @@ def getOtherData():
     workbook.save("VideoData.xlsx")
     workbook.close()
 
-def GenerateWordCloud():
+def GenerateWordCloud(NomeDaFigura):
     
     # lista de stopword
     STOPWORDS_DATA = []
@@ -163,23 +164,27 @@ def GenerateWordCloud():
     all_summary = ' '.join([str(elem) for elem in summary])
     
     # gerar uma wordcloud
-    wc = WordCloud(stopwords=stopwords,
-                          width=1520, height=535, max_font_size=100)
-    
+    wc = WordCloud(stopwords=stopwords,prefer_horizontal=.1,contour_width=5, font_step=2,
+                          width=800, height=600, max_words = 200, max_font_size=40)
     # generate word cloud
     wc.generate(all_summary)
      
     # mostrar a imagem final
-    fig, ax = plt.subplots(figsize=(100,100))
+    fig, ax = plt.subplots(figsize=(1200,1200))
     ax.imshow(wc, interpolation='bilinear')
     ax.set_axis_off()
      
     plt.imshow(wc, interpolation='bilinear');
     
-    # Determine incremented filename
-    a= getNextFilePath("./Pictures")
-    filename = "./Pictures/WordCloud_" +str(a) + ".png"
-    wc.to_file(filename)
+    if NomeDaFigura == "Incremented":
+        # Determine incremented filename
+        a= getNextFilePath("./Pictures")
+        filename = "./Pictures/WordCloud_" +str(a) + ".png"
+        wc.to_file(filename)
+    else:
+        dt = datetime.datetime.today()
+        filename = "./Pictures/" + str(dt.day) + "_" + str(dt.month) + "_" + str(dt.year) + "-" +str(NomeDaFigura) + ".png"
+        wc.to_file(filename)
 
 def getNextFilePath(output_folder):
     highest_num = 0
@@ -193,7 +198,7 @@ def RunAll():
     GetURLsFromYoutube(BuscaSites)
     getSiteList("URL_List.xlsx")
     getTags()
-    GenerateWordCloud()
+    GenerateWordCloud(NomeDaFigura)
 
 
 #BuscaSites = ["https://www.youtube.com/results?search_query=ASPEN+Plus&sp=CAMSBAgFEAE%253D",
@@ -201,5 +206,91 @@ def RunAll():
 #              "https://www.youtube.com/results?search_query=Hysys&sp=CAMSBAgFEAE%253D"] 
 #RunAll()
 
-BuscaSites = ["https://www.youtube.com/results?search_query=OBS&sp=EgIQAQ%253D%253D"]
+#Aprender
+#BuscaSites = ["https://www.youtube.com/channel/UCtFRv9O2AHqOZjjynzrv-xg"]
+#RunAll()
+
+#Marcelo Barros
+#BuscaSites = ["https://www.youtube.com/channel/UCzkg1HCRcmWR-7ZX2sS5LtQ/videos"]
+#RunAll()
+
+#FBGA oficial
+#BuscaSites = ["https://www.youtube.com/c/FBGAoficial/videos"]
+#RunAll()
+
+#NOCAUTE - Blog do Fernando Morais
+#BuscaSites = ["https://www.youtube.com/c/NocauteTV/videos"]
+#RunAll()
+
+#IBREI
+#BuscaSites = ["https://www.youtube.com/channel/UCyP83xHyFqWA7rhxcCQuLQA/videos"]
+#RunAll()
+
+#Finocchio & Ustra
+#BuscaSites = ["https://www.youtube.com/channel/UCBzARAQulJ4JsPm7iyPVfXg/videos"]
+#RunAll()
+
+#Casca Grossa Suprema
+#BuscaSites = ["https://www.youtube.com/c/CascaGrossaSuprema/videos"]
+#RunAll()
+
+# NomeDaFigura = "Notícias sobre negócios"
+# BuscaSites = ["https://www.youtube.com/channel/UCQxtLzG3ckdfsnxd8MDfx2g"]
+# RunAll()
+
+# NomeDaFigura = "Notícias sobre ciência e tecnologia"
+# BuscaSites = ["https://www.youtube.com/channel/UCZ7QHqCYPE3Zi2Tt1sBobEw"]
+# RunAll()
+
+# NomeDaFigura = "Notícias mundiais"
+# BuscaSites = ["https://www.youtube.com/channel/UCvAvFl2OGsuDSoOo93Kd0nA"]
+# RunAll()
+
+# NomeDaFigura = "Notícias nacionais"
+# BuscaSites = ["https://www.youtube.com/channel/UCcE169gw8kJCzyCJZXb7DQw"]
+# RunAll()
+
+# NomeDaFigura = "Notícias sobre saúde"
+# BuscaSites = ["https://www.youtube.com/channel/UCn371zWk5jljg-ycIXkEUSA"]
+# RunAll()
+
+# NomeDaFigura = "Notícias"
+# BuscaSites = ["https://www.youtube.com/channel/UCYfdidRxbB8Qhf0Nx7ioOYw"]
+# RunAll()
+
+# NomeDaFigura = "Ao Vivo"
+# BuscaSites = ["https://www.youtube.com/channel/UC4R8DWoMoI7CAwX8_LjQHig"]
+# RunAll()
+
+# NomeDaFigura = "Em Alta"
+# BuscaSites = ["https://www.youtube.com/feed/trending"]
+# RunAll()
+
+# NomeDaFigura = "Popular on YouTube - Worldwide"
+# BuscaSites = ["https://www.youtube.com/channel/UCgGzSIa8zIsJHbSs0bLplag"]
+# RunAll()
+
+# NomeDaFigura = "Global News"
+# BuscaSites = ["https://www.youtube.com/channel/UChLtXXpo4Ge1ReTEboVvTDg"]
+# RunAll()
+
+# NomeDaFigura = "BETAEQ"
+# BuscaSites = ["https://www.youtube.com/c/BetaEQ/videos"]
+# RunAll()
+
+#NomeDaFigura = "Noticias da Semana"
+#BuscaSites = ["https://www.youtube.com/channel/UCQxtLzG3ckdfsnxd8MDfx2g",
+#              "https://www.youtube.com/channel/UCZ7QHqCYPE3Zi2Tt1sBobEw",
+#              "https://www.youtube.com/channel/UCvAvFl2OGsuDSoOo93Kd0nA",
+#              "https://www.youtube.com/channel/UCcE169gw8kJCzyCJZXb7DQw",
+#              "https://www.youtube.com/channel/UCn371zWk5jljg-ycIXkEUSA",
+#              "https://www.youtube.com/channel/UCYfdidRxbB8Qhf0Nx7ioOYw",
+#              "https://www.youtube.com/channel/UC4R8DWoMoI7CAwX8_LjQHig",
+#              "https://www.youtube.com/feed/trending",
+#              "https://www.youtube.com/channel/UCgGzSIa8zIsJHbSs0bLplag",
+#              "https://www.youtube.com/channel/UChLtXXpo4Ge1ReTEboVvTDg"]
+#RunAll()
+
+NomeDaFigura = "Canal CascaGrossaSuprema"
+BuscaSites = ["https://www.youtube.com/c/CascaGrossaSuprema/videos"]
 RunAll()
